d7875589dd4117133b235b0c8ca440ac83f9f61c
Hernan Cosi <hernan@hernancosi.com>
2022-04-28 01:45:25 -0700

    minidsp 0.1.4 (update formula)

--- Formula/minidsp-rs.rb
+++ Formula/minidsp-rs.rb
@@ -1,6 +1,6 @@
 class MinidspRs < Formula
   desc "MiniDSP Controller"
-  homepage "http://minidsp-rs.pages.dev"
+  homepage "https://minidsp-rs.pages.dev"
   url "https://github.com/mrene/minidsp-rs/archive/refs/tags/v0.1.4.tar.gz"
   sha256 "8b07f7b89807cd8418a7ff81e9f7b6b1d949c2a814f77f4ec87b793f177b04f5"
   license "Apache-2.0"
