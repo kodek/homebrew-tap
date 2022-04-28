5b73c1cba31769e96159d14c69ba3b4cc0b351b4
Hernan Cosi <hernan@hernancosi.com>
2022-04-28 01:29:36 -0700

    Add minidsp-rs formula. Probably won't work as-is.

--- Formula/minidsp-rs.rb
+++ Formula/minidsp-rs.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class MinidspRs < Formula
   desc "MiniDSP Controller"
   homepage "http://minidsp-rs.pages.dev"
@@ -15,15 +12,6 @@ class MinidspRs < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test minidsp-rs`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
